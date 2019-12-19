276a6075ce407347e0b8f1062b1d0b171bbbc6b7
Veronika Dodda <v.shengeliya@gmail.com>
2019-12-19 02:24:38 +0000

    Automatically backed up by Learn

--- first_priority/that_was_unexpected.rb
+++ first_priority/that_was_unexpected.rb
@@ -1,29 +1,32 @@
-class Person
-  attr_accessor :name, :candy_lover
+# class Person
+#   attr_accessor :name, :candy_lover
 
-  def initialize(name, candy_lover)
-    @name = name
-    @candy_lover = candy_lover
-  end
+#   def initialize(name, candy_lover)
+#     @name = name
+#     @candy_lover = candy_lover
+#     if candy_lover [:person]== true
+#   return "Aw sweet, this cat loves them some candy!"
+#   end
 
-  def tall?
-    true
-  end
+#   def tall?
+#     true
+#   end
 
-  def basketball_player?
-    false
-  end
+#   def basketball_player?
+#     false
+#   end
 
-  def likes_candy?
-    true
-  end
-
+#   def likes_candy?
+#     true
+#   end
+require "pry"
   def ballin_candy_lover?
     if @candy_lover == true
       "Aw sweet, this cat loves them some candy!"
     else
       10.times do
         "not a ballin candy lover"
+        binding.pry
     end
   end
 end
--- first_priority/you_called.rb
+++ first_priority/you_called.rb
@@ -1,7 +1,8 @@
+require 'pry'
 
-
-super_mario_party
-
-def super_mario_party
+def super_mario_party (string)
+  
   "Sorry, your princess is in another castle"
+end
+  binding.pry
 end
